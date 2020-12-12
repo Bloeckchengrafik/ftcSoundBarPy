b22a12fc6f49854ad1ef22fe419298ec0336bb8a
Bloeckchengrafik <magiclake@gmx.de>
2020-12-12 14:46:51 +0100

    Added Lib and uploaded to pypi. Just use pip install ftSoundBarCtrl to install

---
+++ ftcSoundBar/build/lib/ftcSoundBar/__init__.py
@@ -0,0 +1,68 @@
+import requests
+import json
+
+class WifiCtrl:
+    def __init__(self, ip):
+        self.__ip = ip
+    def __post(self, endpoint, payload):
+        url = "http://{host}/api/v1/{endpoint}".format(host = self.__ip, endpoint = endpoint)
+
+        headers = {'Content-Type': 'text/json'}
+        payload = json.dumps(payload)
+
+        return requests.request("post", url, headers=headers, data=payload).text
+
+    def __get(self, endpoint, payload):
+        url = "http://{host}/api/v1/{endpoint}".format(host = self.__ip, endpoint = endpoint)
+
+        headers = {'Content-Type': 'text/json'}
+        payload = json.dumps(payload)
+
+        return requests.request("get", url, headers=headers, data=payload).text
+
+    def getTrack(self):
+        request = json.loads(self.__get("track", {}))
+
+        numberTracks = request['tracks']
+        tracks = []
+        for i in range(0, numberTracks):
+            tracks.append((i + 1, request['track#' + str(i)]))
+
+        activeTrack = request['active_track'] + 1
+        state = request['state']
+
+        return tracks, activeTrack, state
+
+    def play(self, id):
+        request = self.__post("track/play", {"track": id})
+        return
+
+    def playPrevious(self):
+        self.__post("track/previous", {})
+        return
+
+    def playNext(self):
+        self.__post("track/next", {})
+        return
+
+    def stopActive(self):
+        self.__post("track/stop", {})
+        return
+
+    def pause(self):
+        self.__post("track/pause", {})
+        return
+
+    def resume(self):
+        self.__post("track/resume", {})
+        return
+
+    def setVolume(self, volume, relative = False):
+        if relative:
+            request = self.__post("volume", {"volume": volume})
+        else:
+            request = self.__post("volume", {"relvolume": volume})
+        return
+
+    def getVolume(self):
+        return int(json.loads(self.__get("volume", {}))["volume"])
---
+++ ftcSoundBar/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='ftSoundBarCtrl',
+    version='1.0',
+    packages=['ftcSoundBar'],
+    url='',
+    license='GNU General Public License v3.0',
+    author='Bloeckchengrafik',
+    author_email='ftpi@gmx.de',
+    description='The ftcSoundBar Python Libary'
+)
